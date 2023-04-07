4c7992bfeeec5aa30dd5c28670ff850780895aa9
Tin Phromchot <tin.phromchot@gmail.com>
2023-04-07 13:55:04 +0700

    update pupeteer get text click and setect option

---
+++ index2.js
@@ -0,0 +1,43 @@
+// import puppeteer เข้ามา
+const puppeteer = require('puppeteer');
+setTimeout(() => {
+    (async () => {
+        // เปิด Browser ขึ้นมา
+        const browser = await puppeteer.launch({ headless: false });
+        // เปิด page ขึ้นมา
+        const page = await browser.newPage();
+        // เข้าเว็บซักเว็บนึง
+        await page.goto('https://getbootstrap.com/docs/5.0/forms/select/');
+
+
+        const f = await page.$('.form-select')
+        await f.select("2")
+        // await page.select('#cars', 'Audi');
+
+        // const n = await page.$("table > tbody > .trline:nth-child(2) > .em")
+        // const t = await (await n.getProperty('textContent')).jsonValue()
+
+        // console.log(t);
+        // const searchResultSelector = "[track='listportal']";
+        // await page.waitForSelector(searchResultSelector);
+        // await page.click(searchResultSelector);
+
+        // ถ้าจะกรอกอะไรก่อนหรือ กดอะไรก่อนก็ทำตรงนี้
+        // page.focus('#search')
+        // page.type('Hello')
+        // page.click('#search_btn')
+
+        // const dimensions = await page.evaluate(() => {
+        //     // code ส่วนนี้จะถูกรันบนหน้าเพจ เหมือนเราไปพิมพ์ใน console 
+        //     console.log(document.documentElement.clientWidth);
+        //     return {
+        //         width: document.documentElement.clientWidth,
+        //         height: document.documentElement.clientHeight,
+        //         deviceScaleFactor: window.devicePixelRatio
+        //     };
+        // });
+        // ปิด Browser 
+        //   await browser.close();
+    })();
+
+}, 0);
